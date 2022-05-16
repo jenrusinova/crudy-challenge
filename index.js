caee5bc3ec8f35171ee5e2bac65be82d4b0b0e6c
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-05-16 12:55:20 -0700

    Created basic route for creating new product

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+var bodyParser = require('body-parser');
+
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/json' }))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
+
+app.post('/addNewProduct', (req, res) => {
+  res.sendStatus(201);
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
