492ad8e2c9471122d42046936724a730e45cd9cc
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-05-16 18:50:11 -0700

    Updated schema

--- schema.sql
+++ schema.sql
@@ -9,3 +9,14 @@ description varchar(100),
 quantity int,
 PRIMARY KEY (productId)
 );
+
+CREATE TABLE deletedProduct (
+productId int AUTO_INCREMENT,
+name varchar(20),
+description varchar(100),
+quantity int,
+user varchar (100),
+comment varchar (256),
+time datetime,
+PRIMARY KEY (productId)
+);
